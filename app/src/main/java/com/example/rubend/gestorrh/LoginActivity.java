2767c9441229964a4fc704d9aa66f62c92f721aa
ruben rodriguez <ruben.rodriguez.rock@gmail.com>
2017-05-24 14:04:08 -0500

    Contenedor de  la interface iniciar sesión

--- app/src/main/java/com/example/rubend/gestorrh/LoginActivity.java
+++ app/src/main/java/com/example/rubend/gestorrh/LoginActivity.java
@@ -1,22 +1,15 @@
 package com.example.rubend.gestorrh;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
 
-public class HomeActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_login);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
-
-    public void inicieSesion(View view){
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
     }
 }
