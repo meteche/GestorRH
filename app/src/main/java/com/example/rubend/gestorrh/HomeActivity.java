64376a88726e87634d220280712e83a40afbf35e
ruben rodriguez <ruben.rodriguez.rock@gmail.com>
2017-05-24 12:22:53 -0500

    Interface inicial

--- app/src/main/java/com/example/rubend/gestorrh/HomeActivity.java
+++ app/src/main/java/com/example/rubend/gestorrh/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.rubend.gestorrh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
